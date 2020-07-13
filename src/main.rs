4f6da6105430c9b292586577ca11e66b40ce2979
alpha-tango-kilo <atk@aaathats3as.com>
2020-07-13 09:59:10 +0100

    Refactored to use enum for cell state

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::io;
 use std::char;
 
+#[derive(Copy, Clone, PartialEq)]
+enum CellState {
+    Empty,
+    Nought,
+    Cross,
+}
+
 fn main() {
     println!("Welcome to Rust Noughts & Crosses!");
 
-    let mut board: [i8; 9] = [0; 9]; // 0 for untaken, 1 for knought, -1 for cross
+    let mut board: [CellState; 9] = [CellState::Empty; 9];
     let mut turn = true;
 
     loop {
@@ -32,10 +39,10 @@ fn main() {
             };
 
             // Validate
-            if !(choice >= 1 && choice <= 9 && board[choice - 1] == 0) {
+            if !(choice >= 1 && choice <= 9 && board[choice - 1] == CellState::Empty) {
                 println!("Invalid number given, please choose a number available on the board");
             } else {
-                board[choice - 1] = if turn { 1 } else { -1 };
+                board[choice - 1] = if turn { CellState::Nought } else { CellState::Cross };
                 break;
             }
         }
@@ -50,15 +57,15 @@ fn main() {
     }
 }
 
-fn check_win(&arr: &[i8; 9]) -> bool {
+fn check_win(arr: &[CellState; 9]) -> bool {
     // Diagonals
-    if (arr[0] != 0 && arr[0] == arr[4] && arr[0] == arr[8]) || (arr[2] != 0 && arr[2] == arr[4] && arr[2] == arr[6]) {
+    if (arr[0] != CellState::Empty && arr[0] == arr[4] && arr[0] == arr[8]) || (arr[2] != CellState::Empty && arr[2] == arr[4] && arr[2] == arr[6]) {
         return true;
     } else {
         for n in 0..2 {
             let offset = n * 3;
             // First clause rows, second clause columns
-            if (arr[offset] != 0 && arr[offset] == arr[offset + 1] && arr[offset] == arr[offset + 2]) || (arr[n] != 0 && arr[n] == arr[n + 3] && arr[n] == arr[n + 6]) {
+            if (arr[offset] != CellState::Empty && arr[offset] == arr[offset + 1] && arr[offset] == arr[offset + 2]) || (arr[n] != CellState::Empty && arr[n] == arr[n + 3] && arr[n] == arr[n + 6]) {
                 return true;
             }
         }
@@ -66,9 +73,15 @@ fn check_win(&arr: &[i8; 9]) -> bool {
     false
 }
 
-fn draw_board(&arr: &[i8; 9]) {
+fn draw_board(arr: &[CellState; 9]) {
     for (n, &cell) in arr.iter().enumerate() {
-        print!(" {} ", if cell == 0 { char::from_digit((n + 1) as u32, 10).expect("Enumeration index was not an int") } else if cell == 1 { 'O' } else { 'X' });
+        let cell_char = match cell {
+            CellState::Empty  => char::from_digit((n + 1) as u32, 10).expect("Enumeration index was not an int"),
+            CellState::Cross  => 'X',
+            CellState::Nought => 'O',
+        };
+        print!(" {} ", cell_char);
+
         if n % 3 != 2 {
             print!("|");
         } else if n != 8 {
